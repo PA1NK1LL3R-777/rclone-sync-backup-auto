ada65182431949f9e62f0536085da3c4ebb1e010
PA1NKII3R <84141021+PA1NK1LL3R-777@users.noreply.github.com>
2022-12-20 09:23:06 -0300

    Create rclone-auto.sh

---
+++ rclone-auto.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+DIR_ORIGEM="/home/$USER/Backup/" # <-- Altere seu diretório de origem
+DIR_DESTINO="mydrive:/"          # <-- Altere seu dispositivo remoto
+
+# Nome do arquivo de log
+LOG="$(date +%m%Y)"
+ARQUIVO_LOG="rlone-sync-$LOG.log"
+
+# Linha com a data do registro
+MENSAGEM_LOG="#$(date "+%A, %d %B %Y") Backup feito com sucesso!#" 
+echo "$MENSAGEM_LOG" >> "$ARQUIVO_LOG" 
+
+# Comando do rclone:
+# -v  Modo verboso
+# -P  Mostra o progresso durante a transferência
+rclone -vP sync --progress "$DIR_ORIGEM" "$DIR_DESTINO" --log-file="$ARQUIVO_LOG"
